c307321b3d08029736e50679872d6c47c3a1f632
Ogbe fidelis <Ogbefidelis2019@gmail.com>
2024-03-09 21:46:58 +0100

    frontend, frist commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
+
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/users";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+  const Layout = ()=>{
+    return (
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: < Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"users",
+          element:<Users/>
+        },
+        {
+          path:"products",
+          element:<Products/>
+        },
+      ] 
+    },
+    
+    
+
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
